63ccce4ba95938779c31f2c354bf9c4ec039e89d
onojastanley <40136094+onojastanley@users.noreply.github.com>
2020-10-01 20:41:51 +0100

    Add files via upload

---
+++ storage/framework/views/4e3f9af79065341cf2d3b6905d87c804ae74f8ed.php
@@ -0,0 +1,11 @@
+<?php $__env->startSection('content'); ?>
+<div class="d-flex justify-content-end mb-2">
+  <a href="" class="btn btn-success">Add Category</a>
+</div>
+
+<div class="card card-default">
+  <div class="card-header">Categories</div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravelcms\resources\views/categories/index.blade.php ENDPATH**/ ?>
